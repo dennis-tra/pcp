4d87cd21ffd841766f2e29577c426334e8f4953a
Dennis Trautwein <git@dtrautwein.eu>
2021-01-22 18:33:01 +0100

    Add receive command documentation

--- pkg/receive/cmd.go
+++ pkg/receive/cmd.go
@@ -15,15 +15,32 @@ import (
 // Command contains the receive sub-command configuration.
 var Command = &cli.Command{
 	Name:    "receive",
+	Usage:   "waits until a peer attempts to connect",
 	Aliases: []string{"r"},
 	Action:  Action,
 	Flags: []cli.Flag{
 		&cli.Int64Flag{
 			Name:    "port",
 			EnvVars: []string{"PCP_PORT"},
+			Aliases: []string{"p"},
+			Usage:   "The port at which you are reachable for other peers in the network.",
 			Value:   44044,
 		},
 	},
+	ArgsUsage: "[DEST_DIR]",
+	UsageText: `DEST_DIR	The destination directory where the received file
+	should be saved. The file will be named as the sender
+	specifies. If no DEST_DIR is given the file will be
+	saved to $XDG_DATA_HOME - usually ~/.data/. If the
+	already exists you will be prompted what you want to do.`,
+	Description: `The receive subcommand starts a multicast DNS service. This
+makes it possible for other peers to discover us - it enables
+peer-to-peer discovery. It is important to note that many
+networks restrict the use of multicasting, which prevents mDNS
+from functioning. Notably, multicast cannot be used in any
+sort of cloud, or shared infrastructure environment. However it
+works well in most office, home, or private infrastructure
+environments.`,
 }
 
 // Action is the function that is called when
