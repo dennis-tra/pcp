504a354e32ae0f0dda7a8d9bf514cf7f660cbc54
Dennis Trautwein <git@dtrautwein.eu>
2021-02-19 19:55:40 +0100

    Use error group for bootstrapping

--- pkg/dht/protocol.go
+++ pkg/dht/protocol.go
@@ -3,15 +3,14 @@ package dht
 import (
 	"sync"
 
-	"github.com/dennis-tra/pcp/pkg/service"
-
 	"github.com/ipfs/go-cid"
-	"github.com/libp2p/go-libp2p-core/peer"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
+	kaddht "github.com/libp2p/go-libp2p-kad-dht"
 	mh "github.com/multiformats/go-multihash"
+	"go.uber.org/atomic"
+	"golang.org/x/sync/errgroup"
 
-	"github.com/dennis-tra/pcp/internal/log"
 	pcpnode "github.com/dennis-tra/pcp/pkg/node"
+	"github.com/dennis-tra/pcp/pkg/service"
 )
 
 // protocol encapsulates the logic for discovering peers
@@ -19,7 +18,7 @@ import (
 type protocol struct {
 	*pcpnode.Node
 	*service.Service
-	init sync.Once
+	bootstrap sync.Once
 }
 
 func newProtocol(node *pcpnode.Node) *protocol {
@@ -27,30 +26,27 @@ func newProtocol(node *pcpnode.Node) *protocol {
 }
 
 // Bootstrap connects to a set of bootstrap nodes to connect
-// to the DHT. This function is a no-op after it is called.
-//
-// TODO: Make bootstrap nodes configurable
-// TODO: Make it possible to bootstrap from local IPFS node
-// TODO: Exit early after we have established min 3 connections
+// to the DHT. This function is a no-op after it is called once.
 func (p *protocol) Bootstrap() error {
-	var err error
-	p.init.Do(func() {
-
-		var wg sync.WaitGroup
-		for _, bp := range dht.GetDefaultBootstrapPeerAddrInfos() {
-			wg.Add(1)
-			go func(pi peer.AddrInfo) {
-				defer wg.Done()
-				if err := p.Connect(p.ServiceContext(), pi); err != nil {
-					log.Infoln("Error connecting to bootstrap peer:", err)
+	errs, ctx := errgroup.WithContext(p.ServiceContext())
+	connCount := atomic.NewInt32(0)
+	p.bootstrap.Do(func() {
+		for _, bp := range kaddht.GetDefaultBootstrapPeerAddrInfos() {
+			errs.Go(func() error {
+				err := p.Connect(ctx, bp)
+				if err == nil {
+					connCount.Inc()
 				}
-			}(bp)
+				return err
+			})
 		}
-
-		wg.Wait()
 	})
-
-	return err
+	err := errs.Wait()
+	// If we have established 3 or more connections don't return an error
+	if err != nil && connCount.Load() < 3 {
+		return err
+	}
+	return nil
 }
 
 func strToCid(str string) (cid.Cid, error) {
