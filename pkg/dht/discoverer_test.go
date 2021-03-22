ccc63808cddc6df7008e8e357fc959e8aa335bf6
Dennis Trautwein <git@dtrautwein.eu>
2021-03-22 21:42:25 +0100

    Increase delays for GitHub actions tests

--- pkg/dht/discoverer_test.go
+++ pkg/dht/discoverer_test.go
@@ -145,7 +145,7 @@ func TestDiscoverer_Discover_restartAsSoonAsCurrentTimeSlotIsExpired(t *testing.
 	ctrl, local, net, teardown := setup(t)
 	defer teardown(t)
 
-	provideTimeout = 20 * time.Millisecond
+	provideTimeout = 100 * time.Millisecond
 
 	mockDefaultBootstrapPeers(t, ctrl, net, local)
 
